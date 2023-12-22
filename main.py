ccf72125d5dc245a444b0fb61caa78cd300b3a4d
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-12-22 09:37:44 +0100

    Update main.py
    added Reponses for if user were to be Angry and Depressed along side happy and sad.

--- main.py
+++ main.py
@@ -33,16 +33,17 @@ def takecommand():
         
     try: 
         print("recognizing...")
-        query = r.recognize_google(audio, language="en-us")
-        print(f"userside {query}\n")
+        User_statment = r.recognize_google(audio, language="en-us")
+        print(f"userside {User_statment}\n")
         
     except Exception as e:
         print("say that agein please...")
         return "None"
-    return query 
+    return User_statment 
 
 def happy_mood_responses():
     responses = [
+        "Tell me more. It sound interesting",
         "I am so happy for you!",
         "That's fantastic news!",
         "You're doing great!",
@@ -52,27 +53,49 @@ def happy_mood_responses():
 
 def sad_mood_responses():
     responses = [
+        "it's ok i know how you may be felling right now",
         "I'm here for you.",
         "It's okay to feel this way.",
         "Let's work through it together.",
         "Take your time, I'm listening."
     ]
     speak(random.choice(responses))
+    
+def angry_mood_responses():
+    responses = [
+        "I understand you're angry right now.",
+        "It's okay to feel angry; let's talk about it.",
+        "I'm here to support you, even when you're angry.",
+        "Let's find a way to channel that anger into something positive.",
+        "Take a deep breath; we can work through this together."
+    ]
+    speak(random.choice(responses))
+
+def depressed_mood_responses():
+    responses = [
+        "I'm sorry you're feeling this way.",
+        "You don't have to go through it alone; I'm here for you.",
+        "Let's take things one step at a time.",
+        "It's okay to ask for help; I'm here to support you.",
+        "Remember, you're not alone in this."
+    ]
+    speak(random.choice(responses))
+
 
 # Add more mood functions like angry_mood_responses(), depressed_mood_responses(), etc.
 
 if __name__ == "__main__":
     wishme()
     while True:
-        query = takecommand().lower()
+        User_statment = takecommand().lower()
 
-        if "happy" in query:
+        if "happy" in User_statment:
             happy_mood_responses()
-        elif "sad" in query:
+        elif "sad" in User_statment:
             sad_mood_responses()
         # Add more conditions for other moods
 
-        elif "youtube" in query:
+        elif "youtube" in User_statment:
             webbrowser.open("youtube.com")
         else:
             print("I can't answer you. I'm sorry.")
