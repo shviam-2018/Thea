bd81e236d65f5efdcac42ebe5f444fb393ef7658
shviam-2018 <shviamjoshi@email.com>
2023-12-30 23:18:38 +0100

    created install.sh for easy install for all

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if Python and pip are installed
+if ! command -v python3 > /dev/null || ! command -v pip3 > /dev/null; then
+    echo "Error: Python and pip are required. Please install them before running this script."
+    exit 1
+fi
+
+# Install specific Python packages
+pip3 install speech_recognition pyttsx3
+
+# Assuming mood is your custom module and is not available on PyPI
+# If mood is available on PyPI, you can install it using pip3 as well
+# Example: pip3 install mood
+
+echo "Installation completed successfully."
+
